231e72bd628837d657c6b10a842ee3007cc8f976
lucindalaurent <lucindalaurent21@gmail.com>
2024-05-07 09:51:44 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -105,10 +105,11 @@ fn main() {
         println!("Luci's computer: done!");
     });
 
+    println!("Luci's Computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
-
+    
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
     executor.run();
